34eca6bf3361f4658d3097b59d8d704c96ed3817
123a6bcw <ga.karandash@yandex.ru>
2018-10-04 03:56:10 +0300

    added homework

--- Parser.hs
+++ Parser.hs
@@ -5,54 +5,83 @@ import Prelude hiding (lookup)
 
 data AST = ASum Operator AST AST
          | AProd Operator AST AST
-         | AAssign Char AST
+         | APow Operator AST AST
+         | AAssign String AST
          | ANum Integer
-         | AIdent Char
+         | AIdent String
+         | AunaryMinus Operator AST
 
 parse :: String -> Maybe AST
 parse input =
   let ts = tokenize input in
   case ts of
     [TEof] -> Nothing
-    _ -> let (tree, ts') = expression ts in
+    _ -> let (tree, ts') = expression True ts in
          if ts' == [TEof]
          then Just tree
          else error ("Parsing error on: " ++ show ts')
 
-expression :: [Token] -> (AST, [Token])
-expression ts =
-  let (termNode, ts') = term ts in
+{-
+  Expression по прежнему обозначает любое выражение
+  High Term - выражение, содержащее в себе только умножение, деление и возведение в степень. Term - только возведение в степень. Тем самым возведение в степень имеет больший приоритет.
+  Переменная типа Bool в expression отвечает за то, может ли первый High Term выражения содержать перед собой унарный минус. В первый Expression в программе передаётся True.
+  Эта переменная передаётся в первый High Term, в остальные передаётся False. Переменная булева типа для Хай терма означает, может ли данный хай терм иметь перед собой унарный минус
+  Внутри High Term передаётся True только в parenthesised expression
+  Таким образом, возможно две ситуации: старший терм начинается с унарного минуса, т.к. является первым термом после скобок или первым термом начального выражения, либо может быть выражение вида
+  (-(expression))
+  Я сделал так, так как это наиболее естественное для меня поведение унарного минуса
+-}
+
+expression :: Bool -> [Token] -> (AST, [Token])
+expression can ts =
+  let (highTermNode, ts') = highTerm can ts in
   case lookup ts' of
     TOp op | op == Plus || op == Minus ->
-      let (exprNode, ts'') = expression $ accept ts' in
-      (ASum op termNode exprNode, ts'')
+      let (exprNode, ts'') = expression False (accept ts') in
+      (ASum op highTermNode exprNode, ts'')
     TAssign ->
-      case termNode of
-        AIdent v -> let (exprNode, ts'') = expression $ accept ts' in
+      case highTermNode of
+        AIdent v -> let (exprNode, ts'') = expression True (accept ts') in
                     (AAssign v exprNode, ts'')
         _ -> error "Syntax error: assignment is only possible to identifiers"
-    _ -> (termNode, ts')
+    _ -> (highTermNode, ts')
+
+highTerm :: Bool -> [Token] -> (AST, [Token])
+highTerm can ts =
+ case lookup ts of
+       TOp op | op == Minus ->
+            if can
+            then let (highTermNode, ts') = highTerm False (accept ts) in 
+                (AunaryMinus Minus highTermNode, ts')
+            else error "Syntax error: wrong usage of unary minus"
+       _ -> 
+            let (termNode, ts') = term ts in
+                case lookup ts' of
+                    TOp op | op == Mult || op == Div ->
+                        let (highTermNode, ts'') = highTerm False (accept ts') in
+                        (AProd op termNode highTermNode, ts'')
+                    _ -> (termNode, ts')
 
 term :: [Token] -> (AST, [Token])
 term ts =
-  let (factNode, ts') = factor ts in
-  case lookup ts' of
-    TOp op | op == Mult || op == Div ->
-      let (termNode, ts'') = term $ accept ts' in
-      (AProd op factNode termNode, ts'')
-    _ -> (factNode, ts')
+         let (factNode, ts') = factor ts in
+          case lookup ts' of
+            TOp op | op == Pow ->
+              let (termNode, ts'') = term (accept ts') in
+              (APow op factNode termNode, ts'')
+            _ -> (factNode, ts')
 
 factor :: [Token] -> (AST, [Token])
 factor ts =
   case lookup ts of
     TLParen ->
-      let (exprNode, ts') = expression $ accept ts in
+      let (exprNode, ts') = expression True (accept ts) in
       case lookup ts' of
         TRParen -> (exprNode, accept ts')
         _ -> error "Syntax error: mismatched parentheses"
     TIdent v -> (AIdent v, accept ts)
-    TDigit d -> (ANum d, accept ts)
-    _ -> error "Syntax error: factor can only be a digit, an identifier or a parenthesised expression"
+    TNumber d -> (ANum d, accept ts)
+    _ -> error "Syntax error: factor can only be a number, an identifier or a parenthesised expression"
 
 lookup :: [Token] -> Token
 lookup = head
@@ -66,13 +95,16 @@ instance Show AST where
       show' n t =
         (if n > 0 then \s -> concat (replicate (n - 1) "| ") ++ "|_" ++ s else id)
         (case t of
-                  ASum  op l r -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
-                  AProd op l r -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
-                  AAssign  v e -> v : " =\n" ++ show' (ident n) e
-                  ANum   i     -> show i
-                  AIdent i     -> show i)
+                  ASum  op l r     -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
+                  AProd op l r     -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
+                  APow op l r     -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
+                  AAssign  v e     -> show v ++ " =\n" ++ show' (ident n) e
+                  ANum   i         -> show i
+                  AIdent i         -> show i
+                  AunaryMinus op t -> showOp op : "\n" ++ show' (ident n) t)
       ident = (+1)
       showOp Plus  = '+'
       showOp Minus = '-'
       showOp Mult  = '*'
+      showOp Pow   = '^'
       showOp Div   = '/'
---
+++ Reverser.hs
@@ -0,0 +1,61 @@
+module Reverser where
+
+import Prelude
+import Tokenizer
+import Parser
+
+{-Эта функция складывает в список ссылки на вершины AST всех факторов на текущем уровне, связанных делением или умножением, и соответствующие операторы, которыми эти термы были связаны.
+Перед тем, как положить ссылку на AST в список, функция также заранее запускает функцию смену ассоциативности в каждой из этих вершин. -}
+
+astProdToList :: AST -> ([Operator], [AST])
+astProdToList  t = case t of
+    AProd op l r ->
+        (op : oper, (changeAssociativity l) : ast) where
+          (oper, ast) = astProdToList r
+    _ ->
+        ([], [t])
+
+
+--данная функция совершает обратную операцию --- по данному списку ссылок на вершины АСТ и операций (деления или умножения), которым они были связаны, восстанавливает АСТ.
+listToAstProd :: ([Operator], [AST]) -> AST 
+listToAstProd ([], y:[]) = y
+listToAstProd (x : xs, y : ys) = AProd x (listToAstProd (xs, ys)) y
+
+
+--тоже самое, только для сложения и вычитания
+astSumToList :: AST -> ([Operator], [AST])
+astSumToList  t = case t of
+    ASum op l r ->
+        (op : oper, (changeAssociativity l) : ast) where
+          (oper, ast) = astSumToList r
+    _ ->
+        ([], [t])
+
+listToAstSum :: ([Operator], [AST]) -> AST 
+listToAstSum ([], y:[]) = y
+listToAstSum (x : xs, y : ys) = ASum x (listToAstSum (xs, ys)) y
+
+{- Если на текущем уровне нет произведения или сложения, функция просто рекурсивно запускает смену ассоциативности от сыновей
+--Иначе она сначала складывает все вершины-термы соответствующей операции в список (первая функция), затем переворачивает получившиеся списки, и затем восстанавливает  дерево по таким спискам (вторая).
+--Несложно понять, что именно этого мы и хотели --- перевернуть вершины так, чтобы на самом нижнем уровне складывались (перемножались) две первые вершины, на уровне повыше на них перемножалась третья вершина и т.д -}
+
+changeAssociativity :: AST -> AST
+changeAssociativity t = case t of 
+                ASum op l r ->
+                    listToAstProd (reverse sums, reverse nodes) where
+                        (sums, nodes) = astSumToList t
+                AProd op l r ->
+                    listToAstProd (reverse prods, reverse nodes) where
+                        (prods, nodes) = astProdToList t
+                APow op l r ->
+                    APow op (changeAssociativity l) (changeAssociativity r)
+                AAssign s l ->
+                    AAssign s (changeAssociativity l)
+                AunaryMinus op l ->
+                    AunaryMinus op (changeAssociativity l)
+                ANum i ->
+                    ANum i
+                AIdent s ->
+                    AIdent s
+                
+             
