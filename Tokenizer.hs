d2240bc1b74e7f6052f8acb88905a0eb2ab3299a
123a6bcw <ga.karandash@yandex.ru>
2018-10-04 04:29:33 +0300

    added HW

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,7 +1,7 @@
 module Tokenizer where
 
-data Token = TDigit Integer
-           | TIdent Char
+data Token = TNumber Integer
+           | TIdent String
            | TOp Operator
            | TLParen
            | TRParen
@@ -13,27 +13,31 @@ data Operator = Plus
               | Minus
               | Mult
               | Div
+              | Pow
               deriving (Show, Eq)
 
 tokenize :: String -> [Token]
 tokenize [] = [TEof]
-tokenize (c : cs) | isOperator c   = TOp (operator c) : tokenize cs
-                  | isDigit c      = TDigit (digit c) : tokenize cs
-                  | isAlpha c      = TIdent (alpha c) : tokenize cs
-                  | c == '('       = TLParen : tokenize cs
-                  | c == ')'       = TRParen : tokenize cs
-                  | c == '='       = TAssign : tokenize cs
-                  | isWhiteSpace c = tokenize cs
-                  | otherwise = error ("Lexical error: unacceptable character " ++ [c])
+tokenize cl@(c : cs) | isOperator c   = TOp (operator c) : tokenize cs
+                    | isDigit c      = let (num, tail) = getNumber cl
+                                       in TNumber num : tokenize tail  
+                    | isAlpha c      = let (name, tail) = getName cl
+                                       in TIdent name : tokenize tail
+                    | c == '('       = TLParen : tokenize cs
+                    | c == ')'       = TRParen : tokenize cs
+                    | c == '='       = TAssign : tokenize cs
+                    | isWhiteSpace c = tokenize cs
+                    | otherwise = error ("Lexical error: unacceptable character " ++ [c])
 
 isOperator :: Char -> Bool
-isOperator x = x `elem` "+-*/"
+isOperator x = x `elem` "+-*/^"
 
 operator :: Char -> Operator
 operator c | c == '+' = Plus
            | c == '-' = Minus
            | c == '*' = Mult
            | c == '/' = Div
+           | c == '^' = Pow
 operator c = error ("Lexical error: " ++ c : " is not an operator!")
 
 isDigit :: Char -> Bool
@@ -60,3 +64,19 @@ alpha c = c
 
 isWhiteSpace :: Char -> Bool
 isWhiteSpace c = c `elem` " \t\n"
+
+getNumber :: String -> (Integer, String)
+--Хочу сразу строить Integer, а не строить промежуточную строку, которую потом нужно переводить в число. Для этого приходится пользоваться аккамулятором
+getNumber = accumulateNumber 0 where
+                accumulateNumber :: Integer -> String -> (Integer, String)
+                accumulateNumber s [] = (s, [])  --Если число находится в конце строки. 
+                                                 --При этом не будет проблемы, что пустая строка будет восприниматься как число, т.к. мы вызываем эту функцию только при обнаружении цифры 
+                accumulateNumber s xl@(x : xs) | isDigit x = accumulateNumber (s * 10 + (digit x)) xs
+                                               | otherwise = (s, xl)
+                        
+getName ::String -> (String, String)
+getName [] = ("", "")
+--Мотивация та же
+getName xl@(x : xs) | isAlpha x = let (name, tail) = getName xs 
+                                  in (x : name, tail)
+                    | otherwise = ([], xl)
